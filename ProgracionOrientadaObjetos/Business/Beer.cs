7cc8236c8bf94f19d6c76472126c6e6059b77ee1
Edison Quimbiamba <exprogresive@gmail.com>
2025-09-07 17:59:09 -0500

    Programacion orientada objetos, contructor y creacion de objetos

---
+++ ProgracionOrientadaObjetos/Business/Beer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgracionOrientadaObjetos.Business
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public string GetInfo()
+        {
+            return $"La cerveza es {Name} y su precio es {Price} pesos";
+        }
+        // metodo contructor 
+
+        public Beer(string nombre, decimal price)
+        {
+            Name = "Desconocida";
+            Price = "0";
+        }
+    }
+}
---
+++ ProgracionOrientadaObjetos/Program.cs
@@ -0,0 +1,28 @@
+﻿using ProgracionOrientadaObjetos.Business;
+using System.Diagnostics;
+/*Beer beer = new Beer()
+{
+    Name = "Corona",
+    Price = "20"
+};// creammos un objeto de tipo beer 
+
+// otra manera de asignar es  agregando valores a cada uno si son publicos 
+ /*var  coronaBeer = new Beer(); 
+ coronaBeer.Name = "Corona";
+coronaBeer.Price = "20";*
+
+Console.WriteLine($"La cerveza es {beer.Name} y su precio es {beer.Price} pesos");
+Console.WriteLine($"La cerveza es {coronaBeer.Name} y su precio es {coronaBeer.Price} pesos");
+
+Console.WriteLine(beer.GetInfo());
+*/
+// podemos tener varis contructores de deistintos tipos 
+// tambine un cosntructor tiene un cuerpo vacio y lo podemos cargar cuando se crean
+// tiene que tener el mismo nombre de la clase 
+
+Beer beer = new Beer("Corona", 20);
+var cornaBeer = new Beer("Corona", 20); 
+
+Console.WriteLine(beer.GetInfo());
+// de testa manera tenemos la certeza de que la cerveza siempre va a tener un nombre y un precio 
+
