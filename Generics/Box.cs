e2ac05456e20b2d9107d0f7a05638f2abb8375d8
Edison Quimbiamba <exprogresive@gmail.com>
2025-10-04 06:17:21 -0500

    Generics  funcionalidad

---
+++ Generics/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Box <T> // ponemos una T, por convension, para indicar que es un tipo generic, son  importantes 
+        //en las convenciones 
+
+    {
+        private readonly T _value; // creamos una variable privada de tipo T  no sabes que sea pero va a ser un tipo
+
+        public Box(T  value) => _value =value; // creamos un constructor que recibe un parametro de tipo T, no se que recibe algo
+
+
+       public  string GetContent() => _value.ToString(); // creamos un metodo que devuelve el contenido de la caja como string
+       
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,9 @@
+﻿//Son un mecanismos que nos permite reutilizar codigo
+using Generics;
+using System.Dynamic;
+
+var box = new Box<int>(12); // creamos una caja de tipo string, como mi clase es generica puedo crear una caja de cualquier tipo
+var box2 = new Box<string>("Hola"); // creamos una caja de tipo string, mirate el constructor y lo entenderas
+
+Console.WriteLine(box.GetContent()); // mostramos el contenido de la caja
+Console.WriteLine(box2.GetContent()); // mostramos el contenido de la caja 
