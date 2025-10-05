1073845882a8c4e1c18e5775a34044eaed62e102
Edison Quimbiamba <exprogresive@gmail.com>
2025-10-05 14:56:42 -0500

    Generics, Actions y  delegados

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -25,3 +25,26 @@ void Show<T>(IRepository<T> repository) // creamos un metodo generico que recibe
         Console.WriteLine(item); // mostramos cada cerveza
     }
 }
+
+// las interfaces genericas son muy utiles para crear repositorios de datos , por que podemos hacer 
+//que funcionen con diferentes tipos de datosl eso nos da una ventaja muy grande o una base de datos
+// por ejemplo podemos tener un repositorio de cervezas , otro de vinos , otro de refrescos etc
+// es algo muy util en la arquitectur12a de software.
+// Los generics no ayudan a no tener  tantas interfaces.
+// por ejemplo si no tuvieramos generics tendriamos que crear una interfaz para cada tipo de dato,
+
+//Delegados es una forma de hacer referencia a un metodo lo podemos dar un tipo y le damos  puede func accion o func.
+
+Operation<int> add = (x, y) => x + y; // creamos una variable de tipo delegado que suma dos enteros
+Operation<double> addDouble = (a, b) => a * b; // creamos una variable de tipo delegado que multiplica dos dobles
+// Podemos definir otro comportamiento
+Operation<string> concat = (s1, s2) => s1 + " " + s2; // creamos una variable de tipo delegado que concatena dos cadenas
+
+Action<IRepository<string>>  s = Show; // creamos una variable de tipo accion que llama al metodo Show
+
+s(beerRepository); // llamamos al metodo Show por medio de la accion
+
+Console.WriteLine(add(3, 4)); // mostramos el resultado de la suma  podemos ejecutarlo como si fuera un metodo normal  por medio de generics    
+Console.WriteLine(addDouble(3.5, 2.0)); // mostramos el resultado de la multiplicacion 
+Console.WriteLine(concat("Hola", "Mundo")); // mostramos el resultado de la concatenacion
+delegate T     Operation<T>(T element1, T element2 ); // creamos un delegado que recibe dos enteros y devuelve un entero
