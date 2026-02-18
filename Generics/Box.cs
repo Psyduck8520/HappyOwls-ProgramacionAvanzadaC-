d9a23ca82ae60d505b05d6aac48be0365b60c384
Edison Quimbiamba G <44550104+Psyduck8520@users.noreply.github.com>
2026-02-17 22:33:30 -0500

    Generics en metodos

--- Generics/Box.cs
+++ Generics/Box.cs
@@ -8,7 +8,7 @@ namespace Generics // 1.Un generico nos permitereutilizar codigo y creamos clase
 {
     public class Box <T> //  esta T es lo recomendado, convenciones son importantes en la programaciones. 
     {
-        private  readonly  T _value; // 2. esta T es un tipo de dato generico, puede ser cualquier tipo de dato, int, string, etc.
+        private    T _value; // 2. esta T es un tipo de dato generico, puede ser cualquier tipo de dato, int, string, etc.
         
         public Box(T value) // 3. el constructor de la clase Box recibe un parametro de tipo T, que es el valor que se va a almacenar en la caja.
         {
@@ -16,6 +16,12 @@ namespace Generics // 1.Un generico nos permitereutilizar codigo y creamos clase
         }
         public  string GetContent()  
             =>  _value.ToString(); // 5. el metodo GetContent devuelve el contenido de la caja como una cadena, utilizando el metodo ToString() del tipo T.
+
+        //para modificar los datos 
+         
+         public T  Get()  => _value; // 12. el metodo Get devuelve el contenido de la caja como el tipo de dato original, en este caso T, que puede ser int, string, etc.
+           
+         public void Set(T value) => _value = value; // 13. el metodo Set recibe un parametro de tipo T, que es el nuevo valor que se va a almacenar en la caja, y lo asigna a la variable _value.
 
 
     }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,11 +1,19 @@
 ﻿using Generics;
 using System.ComponentModel.DataAnnotations;
 
+
 var box1 = new Box<int>(123); // 6. se crea una instancia de la clase Box, especificando el tipo de dato int, y se le asigna el valor 123, tengo que mandar algo de int
+box1.Set(15);
 Console.WriteLine(box1.GetContent()); // 7. se llama al metodo GetContent de la instancia box1, que devuelve el contenido de la caja como una cadena, en este caso "123".
 
 var box2 = new Box<string>("Hola Mundo"); // 8. se crea otra instancia de la clase Box, especificando el tipo de dato string, y se le asigna el valor "Hola Mundo", tengo que mandar algo de string
 Console.WriteLine(box2.GetContent()); // 9. se llama al metodo GetContent de la instancia box2, que devuelve el contenido de la caja como una cadena, en este caso "Hola Mundo".
+var b = box2.Get(); // 14. se llama al metodo Get de la instancia box2, que devuelve el contenido de la caja como el tipo de dato original, en este caso string, y se asigna a la variable b
+Console.WriteLine(b); // 15. se imprime el valor de la variable b, que es "Hola Mundo".
 
 var box3 = new Box<bool>(true); // 10. se crea otra instancia de la clase Box, especificando el tipo de dato DateTime, y se le asigna el valor de la fecha y hora actual, tengo que mandar algo de DateTime
-Console.WriteLine(box3.GetContent()); // 11. se llama al metodo GetContent de la instancia box3, que devuelve el contenido de la caja como una cadena, en este caso la fecha y hora actual.
+Console.WriteLine(box3.GetContent()); // 11. se llama al metodo GetContent de la instancia box3, que devuelve el contenido de la caja como una cadena, en este caso la fecha y hora actual
+
+
+
+
